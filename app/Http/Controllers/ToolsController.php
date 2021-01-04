26da29e0afcaff95cf54db3099afe7b181a6ef26
Rizkika Adam Pratama <rizkikaadam@gmail.com>
2021-01-04 08:28:20 +0700

    make tools and user controller

---
+++ app/Http/Controllers/ToolsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ToolsController extends Controller
+{
+    //
+
+    public function RegisterTools(Request $request)
+    {
+        //function to register tools
+        # code...
+    }
+
+}
---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    //
+
+    public function index()
+    {
+        //retieve User
+        # code...
+    }
+
+    public function add(Request $request)
+    {
+        //add user
+        # code...
+    }
+
+    public function edit()
+    {
+        //edit user
+        # code...
+    }
+
+    public function delete()
+    {
+        //delete user
+        # code...
+    }
+}
